7d0b484e6c8de1b43cbe16928b267417c80fe2a4
csbandurkar <111239768+Chetansbandurkar@users.noreply.github.com>
2026-06-27 10:35:26 +0530

    Time: 50 ms (75.63%), Space: 17.6 MB (95.25%) - LeetHub

--- 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.cpp
+++ 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     long long wonderfulSubstrings(string word) {
-        vector<int> count(1024, 0);
-        long ans = 0;
-        int mask = 0;
-        count[0] = 1;
-
-        for(auto it : word)
-        {
-            mask ^= 1 << (it - 'a');
-            ans += count[mask];
-            for(int i = 0 ; i < 10 ; i++)
-            {
-                ans += count[mask ^ (1 << i)];
+        long long mask = 0;
+        vector<int>count(1024,0);
+        count[0]=1;
+        long long  ans = 0;
+        for(auto s : word){
+            mask ^= 1<<(s-'a');
+            for(int i=0;i<10;i++){
+                ans+=count[mask^(1<<i)];
             }
+            ans+=count[mask];
+
             count[mask]++;
         }
-        return ans;
+
+        return ans ;
     }
 };
