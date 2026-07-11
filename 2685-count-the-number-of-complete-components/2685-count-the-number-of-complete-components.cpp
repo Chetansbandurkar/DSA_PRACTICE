ef746292db81e5a29da145e7e546ac581c3f7033
csbandurkar <111239768+Chetansbandurkar@users.noreply.github.com>
2026-07-11 21:12:52 +0530

    Time: 94 ms (10.94%), Space: 145.4 MB (9.78%) - LeetHub

---
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    void dfs(int node, set<int>& comp, set<int>& vis, vector<vector<int>>& g) {
+        comp.insert(node);
+        vis.insert(node);
+        for (auto it : g[node]) {
+            if (!vis.count(it)) {
+                dfs(it, comp, vis, g);
+            }
+        }
+    }
+    bool isCompleteComponent(set<int>&comp, vector<vector<int>>&g){
+
+        for(auto it : comp){
+            if(g[it].size()!=comp.size()-1) return false;
+        }
+
+        return true;
+    }
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+
+        vector<vector<int>> g(n);
+        for (auto it : edges) {
+            g[it[0]].push_back(it[1]);
+            g[it[1]].push_back(it[0]);
+        }
+
+        int count = 0;
+        set<int>vis;
+        for(int i=0;i<n;i++){
+            if(!vis.count(i)){
+                set<int>comp;
+                dfs(i,comp, vis,g);
+                if(isCompleteComponent(comp, g))count++;
+            }
+        }
+
+        return count;
+    }
+};
