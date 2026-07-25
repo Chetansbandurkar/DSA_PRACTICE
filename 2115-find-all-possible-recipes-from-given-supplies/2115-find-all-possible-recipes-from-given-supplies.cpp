9fdadff25f7b2dee3f1e75d4e6b276f2439487df
csbandurkar <111239768+Chetansbandurkar@users.noreply.github.com>
2026-07-25 14:01:25 +0530

    Time: 119 ms (56.21%), Space: 164.9 MB (36.45%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.cpp
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.cpp
@@ -1,53 +1,36 @@
 class Solution {
 public:
-    vector<string> findAllRecipes(vector<string>& rcp, vector<vector<string>>& ing, vector<string>& s) {
-        unordered_map<string , vector<string>>g;
-        unordered_map<string ,int>ind;
-        unordered_set<string>sup(s.begin(),s.end());
-        unordered_set<string>r(rcp.begin(),rcp.end());
-        unordered_map<string, vector<string>>ingg;
-        queue<string>q;
-        vector<string>ans ;
-        for(int i=0;i<rcp.size();i++){
+    vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
+        unordered_map<string, vector<string>> adj;
+        unordered_map<string, int> inDegree;
 
-            bool isAllIngPresent = true;
-
-            for(auto it:ing[i]){
-                if(!sup.count(it)){
-                    isAllIngPresent= false;
-                }
-                if(r.count(it)){
-                    ind[rcp[i]]++;
-                    g[it].push_back(rcp[i]);
-                }else {
-                    ingg[rcp[i]].push_back(it);
-                }
+        // Step 1: Build the graph and calculate in-degrees
+        for (int i = 0; i < recipes.size(); i++) {
+            for (string ingredient : ingredients[i]) {
+                adj[ingredient].push_back(recipes[i]);
             }
-            if(isAllIngPresent){
-                q.push(rcp[i]);
-            }
+            inDegree[recipes[i]] = ingredients[i].size();
         }
 
-        while(!q.empty()){
-            auto recipe = q.front();
+        // Step 2: Initialize the queue with initial supplies
+        vector<string> result;
+        queue<string> q;
+        for (string supply : supplies) {
+            q.push(supply);
+        }
+
+        // Step 3: BFS to find all possible recipes
+        while (!q.empty()) {
+            string curr = q.front();
             q.pop();
-            ans.push_back(recipe);
-            for(auto it:g[recipe]){
-                ind[it]--;
-                bool canBe = true;
-                for(auto itr :ingg[it]){
-                    if(!sup.count(itr)){
-                        canBe = false;
-                    }
-                }
-
-                if(ind[it]==0 && canBe){
-                    q.push(it);
+            for (string recipe : adj[curr]) {
+                inDegree[recipe]--;
+                if (inDegree[recipe] == 0) {
+                    result.push_back(recipe);
+                    q.push(recipe);
                 }
             }
         }
-
-
-        return ans ;
+        return result;
     }
 };
