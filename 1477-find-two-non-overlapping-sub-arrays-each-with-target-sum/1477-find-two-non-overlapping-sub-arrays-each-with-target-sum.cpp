72af1d22d1dbfa7a2c8405d440417c91db54d9d3
csbandurkar <111239768+Chetansbandurkar@users.noreply.github.com>
2026-09-18 00:31:55 +0530

    Time: 244 ms (18.56%), Space: 174 MB (11.08%) - LeetHub

--- 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
+++ 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
@@ -1,39 +1,37 @@
 class Solution {
 public:
-#define ll long long int
-    int t[100005][3];
-    ll compute(vector<ll>& presum, ll target, int id, ll k, int n) {
-        if (k == 0) {
+#define ll long long
+    ll solve(int ind, int k, int& t, vector<ll>& pres,
+             vector<vector<ll>>& dp) {
+
+        if (k == 0)
             return 0;
-        }
-        if (id >= n) {
+        if (ind >= pres.size()+1)
             return INT_MAX;
+        if (dp[ind][k] != -1)
+            return dp[ind][k];
+
+        ll ans = solve(ind + 1, k, t, pres, dp); // starting again with rem K;
+        auto lb = lower_bound(pres.begin(), pres.end(), pres[ind - 1] + t);
+        if (lb != pres.end() && *lb == pres[ind - 1] + t) {
+            int pos = lb - pres.begin();
+            ans = min(ans, pos - ind + 1 + solve(pos + 1, k - 1, t, pres, dp));
         }
-        if (t[id][k] != -1) {
-            return t[id][k];
-        }
-        ll ans = compute(presum, target, id + 1, k, n);
-        auto lb =
-            lower_bound(presum.begin(), presum.end(), presum[id - 1] + target);
-        if (lb != presum.end() && *lb == presum[id - 1] + target) {
-            int pos = lb - presum.begin();
-            ans = min(ans, pos - id + 1 +
-                               compute(presum, target, pos + 1, k - 1, n));
-        }
-        return t[id][k] = ans;
+
+        return dp[ind][k] = ans;
     }
     int minSumOfLengths(vector<int>& arr, int target) {
         int n = arr.size();
-
-        memset(t, -1, sizeof(t));
-        vector<ll> presum(n + 1, 0);
+        vector<vector<ll>> dp(n + 2, vector<ll>(3, -1));
+        vector<ll> pres(n + 1, 0);
         for (int i = 1; i <= n; i++) {
-            presum[i] = presum[i - 1] + arr[i - 1];
+            pres[i] = pres[i - 1] + arr[i - 1];
         }
-        ll ans = compute(presum, target, 1, 2, n + 1);
-        if (ans >= INT_MAX) {
+
+        ll ans = solve(1, 2, target, pres, dp);
+        if (ans >= INT_MAX)
             return -1;
-        }
+
         return ans;
     }
 };
